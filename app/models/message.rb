39abdb32c8aa0437ecd67056e46956803db1aa66
Nathan Clark <nathan@brandnewbox.com>
2020-12-28 15:03:37 -0600

    Hotwired

--- app/models/message.rb
+++ app/models/message.rb
@@ -1,3 +1,7 @@
 class Message < ApplicationRecord
   belongs_to :room
+  # after_create_commit -> { broadcast_append_to room, partial: "rooms/messages/message" }
+  # after_destroy_commit -> { broadcast_remove_to room }
+  # after_update_commit -> { broadcast_replace_to room, partial: "rooms/messages/message"}
+  broadcasts_to :room, partial: "rooms/messages/message"
 end
